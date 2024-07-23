5c76cfc6e0f2d948a60b2e1d23b38061262da838
carmen <luboy@naver.com>
2024-07-24 00:48:47 +0900

    Upload project

---
+++ Source/MazeHunter/Menu.cpp
@@ -0,0 +1,50 @@
+	// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu.h"
+#include "Components/Button.h"
+
+void UMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+
+	bIsFocusable = true;
+
+	UWorld* World = GetWorld();
+	if (World) {
+		APlayerController* PlayerController = World->GetFirstPlayerController();
+		if (PlayerController) {
+			FInputModeUIOnly UI_InputModeData;
+			UI_InputModeData.SetWidgetToFocus(TakeWidget());
+			UI_InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+			PlayerController->SetInputMode(UI_InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+}
+
+bool UMenu::Initialize()
+{
+	if (!Super::Initialize()) {
+		return false;
+	}
+
+	if (PlayButton) {
+		PlayButton->OnClicked.AddDynamic(this, &ThisClass::PlayButtonClicked);
+	}
+	return true;
+}
+
+void UMenu::PlayButtonClicked()
+{
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Blue,
+			FString(TEXT("Play Button Clicked!"))
+		);
+	}
+}
---
+++ Source/MazeHunter/Menu.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Menu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAZEHUNTER_API UMenu : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void MenuSetup();
+
+protected:
+	virtual bool Initialize() override;
+
+private:
+
+	UPROPERTY( meta = (BindWidget))
+	class UButton* PlayButton;
+	
+	UFUNCTION()
+	void PlayButtonClicked();
+};
