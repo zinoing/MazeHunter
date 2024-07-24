6c0cd9cfa7c93f0a9ceaeab43b516f51b5dd1cf3
carmen <luboy@naver.com>
2024-07-24 15:07:52 +0900

    feat(Character): Completed basic animations

---
+++ Source/MazeHunter/Character/MazeHunterAnimInstance.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MazeHunterAnimInstance.h"
+#include "MazeHunterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UMazeHunterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	MazeHunterCharacter = Cast<AMazeHunterCharacter>(TryGetPawnOwner());
+}
+
+void UMazeHunterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (MazeHunterCharacter == nullptr) {
+		MazeHunterCharacter = Cast<AMazeHunterCharacter>(TryGetPawnOwner());
+	}
+
+	if (MazeHunterCharacter == nullptr) return;
+
+	FVector Velocity = MazeHunterCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+	bIsInAir = MazeHunterCharacter->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = MazeHunterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+}
--- Source/MazeHunter/Character/MazeHunterCharacter.cpp
+++ Source/MazeHunter/Character/MazeHunterCharacter.cpp
@@ -4,18 +4,22 @@
 #include "MazeHunterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AMazeHunterCharacter::AMazeHunterCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(GetMesh());
-	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->TargetArmLength = 400.f;
 	CameraBoom->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 void AMazeHunterCharacter::BeginPlay()
