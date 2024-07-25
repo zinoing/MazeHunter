3224b1fc26b758bd88e2af6ca11483d398dd58b4
carmen <luboy@naver.com>
2024-07-25 21:58:39 +0900

    feat(Item): Created pickup widget

--- Source/MazeHunter/Item/Item.cpp
+++ Source/MazeHunter/Item/Item.cpp
@@ -3,6 +3,8 @@
 
 #include "Item.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "MazeHunter/Character/MazeHunterCharacter.h"
 
 AItem::AItem()
 {
@@ -21,6 +23,9 @@ AItem::AItem()
 	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(RootComponent);
 }
 
 void AItem::BeginPlay()
@@ -30,8 +35,20 @@ void AItem::BeginPlay()
 	if (HasAuthority()) {
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
 	}
 	
+	if (PickupWidget) {
+		PickupWidget->SetVisibility(false);
+	}
+}
+
+void AItem::OnSphereOverlap(UPrimitiveComponent* OVerlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMazeHunterCharacter* MazeHunterCharacter = Cast<AMazeHunterCharacter>(OtherActor);
+	if (MazeHunterCharacter && PickupWidget) {
+		PickupWidget->SetVisibility(true);
+	}
 }
 
 void AItem::Tick(float DeltaTime)
--- Source/MazeHunter/Item/Item.h
+++ Source/MazeHunter/Item/Item.h
@@ -27,6 +27,15 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OVerlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
 private:	
 	UPROPERTY(VisibleAnywhere, Category = "Item Properties")
 	USkeletalMeshComponent* ItemMesh;
@@ -34,6 +43,9 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Item Properties")
 	class USphereComponent* AreaSphere;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Item Properties")
 	EItemState ItemState;
+
+	UPROPERTY(VisibleAnywhere, Category = "Item Properties")
+	class UWidgetComponent* PickupWidget;
 };
